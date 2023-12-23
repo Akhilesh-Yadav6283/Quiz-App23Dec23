89a3970159900f6a1e8822d2f821a396d2d4b850
Akhilesh-Yadav6283 <akhiyadav74085@gmail.com>
2023-12-23 21:46:37 +0530

    my first file

---
+++ script.js
@@ -0,0 +1,10 @@
+let startSolvingBtn = document.getElementById("solvingBtn")
+startSolvingBtn.addEventListener("click",function(){
+    window.open('./index2.html');
+});
+
+ let startQuiz = document.getElementById("startQuizBtn");
+ startQuiz.addEventListener("click", function(){
+    window.open('./index3.html');
+ })
+
---
+++ script2.js
@@ -0,0 +1,4 @@
+let startQuiz = document.getElementById("startQuizBtn");
+startQuiz.addEventListener("click", function(){
+   window.open('./index3.html');
+})
